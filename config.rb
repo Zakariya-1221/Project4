844ee58e1ee535553cc6f4de652c989649d598f1
Iram <masood.39@osu.edu>
2024-10-28 22:29:00 -0400

    message del

--- config.rb
+++ config.rb
@@ -44,3 +44,6 @@ page '/*.txt', layout: false
 #   activate :minify_css
 #   activate :minify_javascript, compressor: Terser.new
 # end
+activate :livereload
+activate :relative_assets
+set :relative_links, true
